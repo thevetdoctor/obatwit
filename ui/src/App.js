65e86beb202da96152f1b709d102bc322344c8e6
thevetdoctor@gmail.com <obafemi oderanti>
2021-12-05 15:13:36 +0100

    bg color switch

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
         {/* <div className="h-full w-full m-0 max-w-sm mx-auto bg-blue-200 rounded-xl shadow-md flex"> */}
         <div className="font-Roboto h-full w-full m-0 max-w-xl max-h-xl my-0 mx-auto bg-blue-300 rounded-xl shadow-md -px-3">
-              <div className="bg-white-300 rounded-xl p-4">
+              <div className="bg-gray-300 rounded-xl p-4">
       <Switch>
               <Route exact path="/" component={Posts} />
               <Route exact path="/twits" component={Twits} />
