2843db7200c1e3960fe067d112ab37b5c36eee5e
thevetdoctor@gmail.com <obafemi oderanti>
2021-12-05 09:46:18 +0100

    check window in sw registration

--- ui/src/service-worker.js
+++ ui/src/service-worker.js
@@ -128,7 +128,8 @@ function checkValidServiceWorker(swUrl, config) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
-            window.location.reload();
+            if(window) window.location.reload();
+           
           });
         });
       } else {
