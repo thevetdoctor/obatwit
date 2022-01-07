09de70afa90b6adf92c46d41d0ae05e6d41e27b9
thevetdoctor@gmail.com <obafemi oderanti>
2022-01-07 20:46:57 +0100

    attach image capture

--- ui/src/components/AttachImage.js
+++ ui/src/components/AttachImage.js
@@ -42,7 +42,7 @@ export default function AttachImage({imageUrl, setImageUrl}) {
                 <input
                     type="file"
                     placeholder=""
-                    accept="image/*;capture=camera"
+                    accept="image/*"
                     capture
                     className="hidden"
                     onChange={e => handleImage(e)}
