f0d7e4cb02626eaaae3553f8aa5c1bccc3675834
thevetdoctor@gmail.com <obafemi oderanti>
2022-01-14 11:25:13 +0100

    fix cors bug

--- index.js
+++ index.js
@@ -21,35 +21,36 @@ app.use((req, res, next) => {
     next();
 })
 app.use(express.static(path.join(__dirname, './ui/build')));
-app.use(express.static(path.join(__dirname, 'client')));
+// app.use(express.static(path.join(__dirname, 'client')));
 
 const {PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY} = process.env;
 
 webPush.setVapidDetails('mailto:thevetdoctor@gmail.com', PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY);
 
+
+// app.post('/subscribe', (req, res) => {
+    //     const subscription = req.body;
+    
+    //     res.status(201).json({});
+    
+    //     const payload = JSON.stringify({ title: `Push Twitee from server @ ${req.protocol}://${req.hostname}:${port}` });
+    //     console.log('server push response')
+    //     webPush.sendNotification(subscription, payload).catch(error => console.log(error));
+    // });
+    
+    // Handles all errors
+    app.use((err, req, res, next) => {
+        if (process.env.NODE_ENV === 'production') {
+            return res.status(err.status || 400).send({ success: false });
+        }
+        if (err) console.log(err);
+        return res
+        .status(err.status || 400)
+        .send({ success: false, message: err.message });
+    });
+
 routeHandler(app);
-
-app.post('/subscribe', (req, res) => {
-    const subscription = req.body;
-
-    res.status(201).json({});
-
-    const payload = JSON.stringify({ title: `Push Twitee from server @ ${req.protocol}://${req.hostname}:${port}` });
-    console.log('server push response')
-    webPush.sendNotification(subscription, payload).catch(error => console.log(error));
-});
-
-// Handles all errors
-app.use((err, req, res, next) => {
-    if (process.env.NODE_ENV === 'production') {
-      return res.status(err.status || 400).send({ success: false });
-    }
-    if (err) console.log(err);
-    return res
-    .status(err.status || 400)
-    .send({ success: false, message: err.message });
-});
-
+    
 app.get('/', (req, res) => {
     // return res.status(200).json({
     //     message: 'Welcome to Twitee'
