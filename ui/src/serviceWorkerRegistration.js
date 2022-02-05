025c06b71ab629fbad07a203fe84a520ba7a2bd4
thevetdoctor@gmail.com <obafemi oderanti>
2022-02-05 07:51:41 +0100

    vapid env variable undefined

--- ui/src/serviceWorkerRegistration.js
+++ ui/src/serviceWorkerRegistration.js
@@ -38,7 +38,7 @@ const isLocalhost = Boolean(
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(process.env.REACT_APP_PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -47,7 +47,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.REACT_APP_PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
@@ -81,7 +81,7 @@ function registerValidSW(swUrl, config) {
   
       const subscription = await registration.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(process.env.PUBLIC_VAPID)
+          applicationServerKey: urlBase64ToUint8Array(process.env.REACT_APP_PUBLIC_VAPID)
       });
       console.log('Push Manager registered');
   
